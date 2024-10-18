cf4430526e5a695dc38c66054f7dd7af1b9912d4
JoschaJ <jjahns@mpifr-bonn.mpg.de>
2024-10-18 13:37:44 +0800

    version -> 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='injspection',
-      version='0.0.1',
+      version='0.0.2',
       url='https://github.com/askap-craco/injspection',
       author='Joscha Jahns-Schindler',
       author_email='jjahnsschindler@swin.edu.au',
